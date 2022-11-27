981d2ecd682adf7fe5b1c2f5b13565f522f1828c
f3iru <this.fairuz@gmail.com>
2022-11-27 16:06:04 +0700

    Fixed url declaring delay

--- lib/webview/webview_screen.dart
+++ lib/webview/webview_screen.dart
@@ -11,59 +11,59 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   String url = "https://flask-facerec.herokuapp.com/login";
-  int nis = 0;
-  int nis2 = 2021118576;
-  String pass2 = 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f';
-  int id = 0;
-  String pass = "not changed";
 
-  Future<dynamic> getFromSession(String key) async {
-    return await SessionManager().get(key);
+  late int id;
+  late String password;
+
+  Future<int> fetchId() async {
+    return await SessionManager().get("user");
   }
 
-  Future<int> asyncNIS() async {
-    return await SessionManager().get("user");
+  Future<String> fetchPassword() async {
+    return await SessionManager().get("pass");
   }
 
   @override
   Widget build(BuildContext context) {
-    asyncNIS().then((value) {
-      setState(() {
-        this.nis = value;
-      });
-    });
 
-    getFromSession("user").then((res) {
+    fetchId().then((res) {
       setState(() {
         this.id = res;
       });
     });
 
-    getFromSession("pass").then((res) {
+    fetchPassword().then((res) {
       setState(() {
-        this.pass = res;
+        this.password = res;
       });
     });
 
-    String with_get_params = "?id=$id";
-    with_get_params += "&w=$pass";
-    // print(id);
-    // print(pass);
-    print(id);
-    print(nis2);
-    print(pass2);
-    print(pass);
+    url = "$url?id=$id&w=$password";
 
-
-    return Scaffold(
-
+    return id != null ? Scaffold(
       body: WillPopScope(
         onWillPop: () async {
           return true;
         },
         child: WebView(
           javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: "$url?id=$nis2&w=$pass2",
+          initialUrl: url,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+      ),
+    ) : Scaffold(
+      body: WillPopScope(
+        onWillPop: () async {
+          return true;
+        },
+        child: const WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: "",
         ),
       ),
       floatingActionButton: FloatingActionButton(
